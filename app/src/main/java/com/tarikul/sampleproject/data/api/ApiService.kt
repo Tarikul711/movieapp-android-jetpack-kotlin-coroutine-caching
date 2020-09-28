c9451eca1910a4d399044b20854624ee45ffec9a
Tarikul711 <Tarikul711@gmail.com>
2020-09-28 22:23:49 +0600

    Modify the ApiService.kt

--- app/src/main/java/com/tarikul/sampleproject/data/api/ApiService.kt
+++ app/src/main/java/com/tarikul/sampleproject/data/api/ApiService.kt
@@ -1,10 +1,17 @@
 package com.tos.myapplication.data.api
 
 
+import com.tarikul.sampleproject.data.api.Endpoint.DISCOVER_MOVIE
+import com.tarikul.sampleproject.data.api.Endpoint.DISCOVER_TV
+import com.tarikul.sampleproject.data.model.movies.MovieResponse
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("photos")
-    suspend fun getPhotoAlbum():List<PhotoModel>
+
+    @GET(DISCOVER_MOVIE)
+    suspend fun getMovies(): MovieResponse
+
+    @GET(DISCOVER_TV)
+    suspend fun getTvShows(): MovieResponse
 
 }
