613436cc2cdf640180e92628fd177a149b55046f
Tarikul711 <Tarikul711@gmail.com>
2020-09-29 09:51:55 +0600

    Implement jetpack Navigation

--- app/src/main/java/com/tarikul/sampleproject/ui/main/view/fragment/MovieFragment.kt
+++ app/src/main/java/com/tarikul/sampleproject/ui/main/view/fragment/MovieFragment.kt
@@ -1,10 +1,11 @@
-package com.tarikul.sampleproject
+package com.tarikul.sampleproject.ui.main.view.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tarikul.sampleproject.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
