bc10ba1b24cbfeb2ff96eff9e092dd032868f74a
Tarikul711 <Tarikul711@gmail.com>
2020-09-29 09:51:12 +0600

    Include all fragment for the main screen

--- app/src/main/java/com/tarikul/sampleproject/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/tarikul/sampleproject/ui/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tarikul.sampleproject
+package com.tarikul.sampleproject.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tarikul.sampleproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
