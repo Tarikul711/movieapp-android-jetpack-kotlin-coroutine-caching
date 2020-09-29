fe9a5d11eb7bf16db1c28eea3093ccbe8e2bacfd
Tarikul711 <Tarikul711@gmail.com>
2020-09-29 10:33:34 +0600

    Config the nav view with the navigation component

--- app/src/main/java/com/tarikul/sampleproject/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/tarikul/sampleproject/ui/main/view/MainActivity.kt
@@ -2,11 +2,23 @@ package com.tarikul.sampleproject.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.tarikul.sampleproject.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.fragment)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
