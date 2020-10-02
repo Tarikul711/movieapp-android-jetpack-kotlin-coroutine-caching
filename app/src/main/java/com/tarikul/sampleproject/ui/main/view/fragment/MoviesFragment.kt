bab25eb2d1e21fe974667d8f1da8fa1923d98545
Tarikul711 <Tarikul711@gmail.com>
2020-10-03 00:30:51 +0600

    Controlling the navigation

--- app/src/main/java/com/tarikul/sampleproject/ui/main/view/fragment/MoviesFragment.kt
+++ app/src/main/java/com/tarikul/sampleproject/ui/main/view/fragment/MoviesFragment.kt
@@ -1,26 +1,31 @@
 package com.tarikul.sampleproject.ui.main.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.tarikul.sampleproject.R
-import kotlinx.android.synthetic.main.fragment_movies.*
+import kotlinx.android.synthetic.main.fragment_movies.view.*
 
 
 class MoviesFragment : Fragment() {
 
-    val args: MoviesFragmentArgs by navArgs()
+    private val args: MoviesFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_movies, container, false)
-        tvTitle.text = args.movieType.toString()
+        setupView(view)
         return view
+    }
+
+    private fun setupView(view: View) {
+        view.apply {
+            tvTitleText.text = args.movieType.toString()
+        }
     }
 }
