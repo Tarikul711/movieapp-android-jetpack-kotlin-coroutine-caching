6d754ea10451e1554c5771f07f24b8eca59b99e5
Tarikul711 <Tarikul711@gmail.com>
2020-10-02 20:27:43 +0600

    Modify the AppConfig

--- app/src/main/java/com/tarikul/sampleproject/data/api/ApiConfig.kt
+++ app/src/main/java/com/tarikul/sampleproject/data/api/ApiConfig.kt
@@ -22,11 +22,16 @@ object QueryMovie {
     const val LANGUAGE = "language"
     const val INCLUDE_ADULT = "include_adult"
     const val INCLUDE_VIDEO = "include_video"
+    const val TIMEZONE = "timezone"
+    const val INCLUDE_NULL_FIRST = "include_null_first_air_dates"
+
 
     const val SORT_BY_DEFAULT = "popularity.desc"
     const val LANGUAGE_DEFAULT = "en-US"
+    const val TIMEZONE_DEFAULT = "America%2FNew_York"
     const val INCLUDE_ADULT_DEFAULT = false
     const val INCLUDE_VIDEO_DEFAULT = false
+    const val INCLUDE_NULL_FIRST_DEFAULT = false
 
     const val API_KEY_VALUE = "c4519d9993a9c4b6576e45741333593a"
 }
