8a91d1378b1fefb20be7a051cb5f8115a27afc42
Tarikul711 <Tarikul711@gmail.com>
2020-10-02 20:36:13 +0600

    TvShow Api Implementation

--- app/src/main/java/com/tarikul/sampleproject/data/api/ApiHelper.kt
+++ app/src/main/java/com/tarikul/sampleproject/data/api/ApiHelper.kt
@@ -2,11 +2,13 @@ package com.tos.myapplication.data.api
 
 import com.tarikul.sampleproject.data.model.movies.MovieResponse
 import com.tarikul.sampleproject.data.model.trending.TrendingResponse
+import com.tarikul.sampleproject.data.model.tvShows.TvShowResponse
 import kotlinx.coroutines.flow.Flow
 
 interface ApiHelper {
 
     suspend fun getMovies(): Flow<MovieResponse>
     suspend fun getTrendingMovies(): Flow<TrendingResponse>
+    suspend fun getTvShows(): Flow<TvShowResponse>
 
 }
--- app/src/main/java/com/tarikul/sampleproject/data/api/ApiHelperImpl.kt
+++ app/src/main/java/com/tarikul/sampleproject/data/api/ApiHelperImpl.kt
@@ -2,6 +2,7 @@ package com.tos.androidlivedataviewmodel.projectOne.data.api
 
 import com.tarikul.sampleproject.data.model.movies.MovieResponse
 import com.tarikul.sampleproject.data.model.trending.TrendingResponse
+import com.tarikul.sampleproject.data.model.tvShows.TvShowResponse
 import com.tos.myapplication.data.api.ApiHelper
 import com.tos.myapplication.data.api.ApiService
 import kotlinx.coroutines.flow.Flow
@@ -17,6 +18,9 @@ class ApiHelperImpl(var apiService: ApiService, var page: Int) : ApiHelper {
 
     override suspend fun getTrendingMovies(): Flow<TrendingResponse> =
         flow { emit(apiService.getTrendingMovies("day")) }
+
+    override suspend fun getTvShows(): Flow<TvShowResponse> =
+        flow { emit(apiService.getTvShow(page)) }
 
 
 }
