f73be16752ea976d92f8631965fffdff78f6c2dc
Tarikul711 <Tarikul711@gmail.com>
2020-10-04 11:36:14 +0600

    Modify the MovieDetailFragment.kt class

--- app/src/main/java/com/tarikul/sampleproject/ui/main/view/fragment/MovieDetailFragment.kt
+++ app/src/main/java/com/tarikul/sampleproject/ui/main/view/fragment/MovieDetailFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.tarikul.sampleproject.R
+import com.tarikul.sampleproject.data.model.movie.MovieResponse
 import com.tarikul.sampleproject.ui.base.ViewModelFactory
 import com.tarikul.sampleproject.ui.main.viewmodel.HomeFragmentViewModel
 import com.tarikul.sampleproject.ui.main.viewmodel.MovieDetailViewModel
 import com.tos.androidlivedataviewmodel.projectOne.data.api.ApiHelperImpl
+import com.tos.androidlivedataviewmodel.projectOne.utils.Status
 import com.tos.myapplication.data.api.RetrofitBuilder
+import kotlinx.android.synthetic.main.fragment_movie_detail.view.*
 
 class MovieDetailFragment : Fragment() {
 
@@ -25,11 +29,10 @@ class MovieDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_movie_detail, container, false)
         setupViewModel()
-
+        setupMovieDetailObserver(view)
         return view
     }
 
-    private fun setupUI() {}
     private fun setupViewModel() {
         movieDetailViewModel =
             ViewModelProvider(
@@ -46,8 +49,29 @@ class MovieDetailFragment : Fragment() {
                 )
     }
 
-    private fun setupMovieDetailObserver() {
+    private fun setupMovieDetailObserver(view: View) {
+        activity?.let {
+            movieDetailViewModel.getMovieDetail().observe(it, Observer {
+                it?.let { resource ->
+                    when (resource.status) {
+                        Status.SUCCESS -> {
+                            resource.data?.let { movie -> setupUI(movie, view) }
+                        }
+                        Status.ERROR -> {
+                        }
+                        Status.LOADING -> {
+                        }
+                    }
 
+                }
+            })
+        }
+    }
+
+    private fun setupUI(movie: MovieResponse, view: View) {
+        view.apply {
+            tvMovieTitle.text = movie.original_title
+        }
     }
 
 }
